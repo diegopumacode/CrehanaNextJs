7c911b8baea46c7256173596497acac44f8e2f9e
diegopumacode <diegopumacode@gmail.com>
2021-03-02 23:32:06 -0500

    adding loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import NextNProgress from '../utils/progressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <NextNProgress
+      color="#4B22F4"
+      height="5"
+    />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
